6e8f1b70295d4e7db0edcb278a76530f31742319
krizk93 <karim.rizk@live.com>
2017-12-17 22:13:43 +0100

    using getLastKnownLocation
    previous method seemed to only get current location after device moves (onLocationChanged)

--- app/src/main/java/com/stormy/MainActivity.java
+++ app/src/main/java/com/stormy/MainActivity.java
@@ -9,56 +9,30 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LocationListener{
 
-    private TextView tv_City,
-            tv_Date,
-            tv_PrecipitationProbability,
-            tv_Temperature,
-            tv_ApparentTemperature;
     TextView mShowLocationTV;
     LocationManager locationManager;
     String locationProvider;
+    Location mLocation;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
-        //Setting the action bar with our custom view:
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setElevation(0);
-
-        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v = inflater.inflate(R.layout.layout_for_actionbar, null);
-
-        actionBar.setCustomView(v);
-
-        //Getting the views in the actionBar
-        tv_City = actionBar.getCustomView().findViewById(R.id.text_city);
-        tv_Date = actionBar.getCustomView().findViewById(R.id.text_date);
-
-
-        tv_PrecipitationProbability = findViewById(R.id.text_precipitation_chance);
-        tv_Temperature = findViewById(R.id.text_temperature_current);
-        tv_ApparentTemperature = findViewById(R.id.text_apparent_temperature);
-
-
-       // mShowLocationTV = (TextView) findViewById(R.id.location_Text);
+        mShowLocationTV = (TextView) findViewById(R.id.text_location);
 
         //request in app permissions
+
         if (ContextCompat
                     .checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED
@@ -75,21 +49,27 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
     @Override
     protected void onResume() {
         super.onResume();
-        getLocation();
+        mLocation = getLocation();
+        if (mLocation != null){
+            mShowLocationTV.setText("Initial Current Location. Latitude: " + mLocation.getLatitude() + "\n Longitude: " + mLocation.getLongitude());
+        }
     }
 
-    void getLocation(){
+    Location getLocation(){
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Criteria locationCritera = new Criteria();
         locationCritera.setAccuracy(Criteria.ACCURACY_FINE);
         locationCritera.setPowerRequirement(Criteria.POWER_LOW);
+        Location location = null;
 
         locationProvider = locationManager.getBestProvider(locationCritera,true);
-        if (locationProvider != null) {
+
+        if (locationProvider != null && locationManager.isProviderEnabled(locationProvider)) {
             try {
 
                 locationManager.requestLocationUpdates(locationProvider, 60 * 1000, 0, (LocationListener) this);
+                location = locationManager.getLastKnownLocation(locationProvider);
                 Toast.makeText(this, "Best Provider is " + locationProvider, Toast.LENGTH_LONG).show();
             /*TODO the requestLocationUpdates methods is set to check location updates every minute. We should find a reasonable interval to balance performance and battery consumption
             */
@@ -98,11 +78,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
                 e.printStackTrace();
             }
         }
+
+        return location;
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        //mShowLocationTV.setText("Latitude: " + location.getLatitude() + "\n Longitude: " + location.getLongitude());
+        mShowLocationTV.setText("Latitude: " + location.getLatitude() + "\n Longitude: " + location.getLongitude());
 
 
     }
